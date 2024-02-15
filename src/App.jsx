87419c6c1e0b1dd5da07ec1bf442e37c7484b5af
Jeimy Jara Desarrollador 1 <desarrollador1@imporfactoryusa.com>
2024-02-15 10:41:29 -0500

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="h-screen bg-blue-500">
+        <div className="flex justify-center items-center h-full">
+          <h1 className="text-white text-4xl">Hello World</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
